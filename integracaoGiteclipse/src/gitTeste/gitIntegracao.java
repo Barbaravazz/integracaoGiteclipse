2638c60fbade8409a4304483becf376987b015e7
Barbaravazz <barbaraborgesvaz@gmail.com>
2024-07-03 15:09:08 -0300

    Update gitIntegracao.java

--- integracaoGiteclipse/src/gitTeste/gitIntegracao.java
+++ integracaoGiteclipse/src/gitTeste/gitIntegracao.java
@@ -5,6 +5,7 @@ public class gitIntegracao {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("teste");
+		System.out.println("teste2");
 	}
 
 }
